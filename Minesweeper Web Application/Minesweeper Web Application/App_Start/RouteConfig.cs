feff19db35c3c3abf1e4e5ecd5cf014b8d96121e
cmdeao <cmdeao16@gmail.com>
2020-08-11 16:10:30 -0500

    Created the controllers, model, and services.

--- Minesweeper Web Application/Minesweeper Web Application/App_Start/RouteConfig.cs
+++ Minesweeper Web Application/Minesweeper Web Application/App_Start/RouteConfig.cs
@@ -14,6 +14,18 @@ namespace Minesweeper_Web_Application
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Login",
+                url: "{Login}",
+                defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Registration",
+                url: "{Registration}/{Index}",
+                defaults: new { controller = "Registration", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
